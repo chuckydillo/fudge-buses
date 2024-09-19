2ef1b59b7428f030be8c9cde9ccedf5f664a8703
Charles Mould <charles.mould.cm@gmail.com>
2024-09-19 14:59:34 +0000

    create JS file for navbar. Copy and edit script from trinity project

---
+++ js/navbar.js
@@ -0,0 +1,28 @@
+console.log("nav bar connected")
+
+//navbar
+
+document.addEventListener('DOMContentLoaded', function() {
+    const navContainer = document.getElementById('navbar-container');
+    const indexNavContainer = document.getElementById('index-navbar-container');
+
+
+    if (userNavContainer) {
+        fetch('pages/navbar-guest.html')
+        .then(response => response.text())
+        .then(data => {
+            indexNavContainer.innerHTML = data;
+        })
+        .catch(error => console.error('Error loading the navbar:', error));
+    }
+
+    if (guestNavContainer) {
+        fetch('pages/navbar-user.html')
+        .then(response => response.text())
+        .then(data => {
+            navContainer.innerHTML = data;
+        })
+        .catch(error => console.error('Error loading the navbar:', error));
+    }
+
+});
