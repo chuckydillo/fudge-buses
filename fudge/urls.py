789ae1f18563dc50118955eb793fbdc9b423b160
Charles Mould <charles.mould.cm@gmail.com>
2024-09-24 12:50:35 +0000

    add paths for register, login, logout

--- fudge/urls.py
+++ fudge/urls.py
@@ -1,7 +1,12 @@
 from . import views
 from django.urls import path
+from .views import register
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', 
         views.HomePage.as_view(), name='home'),
+        path('register/', register, name='register'),
+        path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+        path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
