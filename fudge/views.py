e1c30494e413f1bf047a20ed1b27d44d5497988b
Charles Mould <charles.mould.cm@gmail.com>
2024-09-24 16:08:28 +0000

    fix urls

--- fudge/views.py
+++ fudge/views.py
@@ -1,25 +1,28 @@
-from django.shortcuts import render, redirect
+#from django.shortcuts import render, redirect
 # Create your views here.
 
+#from django.views.generic import TemplateView
+#from django.contrib.auth import login
+#from .forms import CustomUserCreationForm
+
+from django.shortcuts import render
 from django.views.generic import TemplateView
-from django.contrib.auth import login
-from .forms import CustomUserCreationForm
 
 class HomePage(TemplateView):
     """
-    Displays home page
+    Displays home page"
     """
     template_name = 'index.html'
 
 
-def register(request):
-    if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('index')
-    else:
-        form = CustomUserCreationForm()
-
-    return render(request, 'registration/register.html', {'form': form})
+#def register(request):
+#    if request.method == 'POST':
+#        form = CustomUserCreationForm(request.POST)
+#        if form.is_valid():
+#            user = form.save()
+#            login(request, user)
+#            return redirect('index')
+#    else:
+#        form = CustomUserCreationForm()
+#
+#    return render(request, 'registration/register.html', {'form': form})
--- fudgedb/urls.py
+++ fudgedb/urls.py
@@ -29,5 +29,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('fudge.urls'), name='home'),
-
 ]
