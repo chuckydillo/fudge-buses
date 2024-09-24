f7937b70597a90db7b61fa011fab33d17f3c4cf2
Charles Mould <charles.mould.cm@gmail.com>
2024-09-24 13:25:40 +0000

    debug false

--- fudgedb/settings.py
+++ fudgedb/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "8000-chuckydillo-fudgebuses-ch6b2s6etz8.ws.codeinstitute-ide.net",
