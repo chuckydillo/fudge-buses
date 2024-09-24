a5ef05fda484e57d339cf7ef43dad8db8eb97440
Charles Mould <charles.mould.cm@gmail.com>
2024-09-24 15:55:16 +0000

    fix missing bracket

--- fudge/urls.py
+++ fudge/urls.py
@@ -7,3 +7,4 @@ urlpatterns = [
     path('register/', register, name='register'),  # Register page
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),  # Login page
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),  # Logout page
+]
