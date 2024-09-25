07d9322dc14228496a8889bf450079c682e68aae
Charles Mould <charles.mould.cm@gmail.com>
2024-09-25 10:03:23 +0000

    removed comments from function

--- fudge/views.py
+++ fudge/views.py
@@ -15,14 +15,14 @@ class HomePage(TemplateView):
     template_name = 'index.html'
 
 
-#def register(request):
-#    if request.method == 'POST':
-#        form = CustomUserCreationForm(request.POST)
-#        if form.is_valid():
-#            user = form.save()
-#            login(request, user)
-#            return redirect('index')
-#    else:
-#        form = CustomUserCreationForm()
-#
-#    return render(request, 'registration/register.html', {'form': form})
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+    else:
+        form = CustomUserCreationForm()
+
+    return render(request, 'registration/register.html', {'form': form})
