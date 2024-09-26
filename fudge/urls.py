880d12e42d288689b3d28cf152269f8066d63676
Charles Mould <charles.mould.cm@gmail.com>
2024-09-26 10:36:54 +0000

    fix broken path

--- fudge/urls.py
+++ fudge/urls.py
@@ -18,6 +18,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.HomePage.as_view(), name='home'),
     path('register/', register, name='register'),  # Register page
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),  # Login page
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),  # Login page
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),  # Logout page
 ]
