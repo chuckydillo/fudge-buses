4affd911400c4f5e222e5b205eace7256469569e
Charles Mould <charles.mould.cm@gmail.com>
2024-09-26 15:03:56 +0000

    add view for bus report form, add comments on code

--- fudge/views.py
+++ fudge/views.py
@@ -3,17 +3,19 @@
 
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
-from django.contrib.auth import login
-from .forms import CustomUserCreationForm
+from django.contrib.auth import login 
+from .forms import CustomUserCreationForm # For user registratin
 from django.contrib.auth.decorators import login_required
 
+
+# View for homepage
 class HomePage(TemplateView):
     """
     Displays home page"
     """
     template_name = 'index.html'
 
-
+# View for user registration
 def register(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
@@ -26,5 +28,14 @@ def register(request):
 
     return render(request, 'accounts/register.html', {'form': form})
 
+# View for profile
 def profile_view(request):
     return render(request, 'accounts/profile.html', {'user': request.user})
+
+# View for Bus Report Form
+def busreport_view(request):
+    return render(request, 'bus/bus_report.html')
+
+
+
+
