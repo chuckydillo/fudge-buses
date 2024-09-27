4430e482474b3442a92581249dc254a36c821362
Charles Mould <charles.mould.cm@gmail.com>
2024-09-27 14:13:43 +0000

    change model name

--- fudge/models.py
+++ fudge/models.py
@@ -10,7 +10,7 @@ class BusInfoModel(models.Model):
     bus_company = models.CharField(max_length=200)
     bus_number = models.CharField(max_length=5)
 
-class BusStopTime(models.Model):
+class BusStops(models.Model):
     bus_info = models.ForeignKey(BusInfoModel, related_name="stops", on_delete=models.CASCADE)
     bus_stop = models.CharField(max_length=1000)
     bus_time = models.TimeField()
