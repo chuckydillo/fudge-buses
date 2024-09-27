e3d0250d36e2f580906b1db8ae21a533472eb0bc
Charles Mould <charles.mould.cm@gmail.com>
2024-09-27 12:05:20 +0000

    add model for businfo

--- fudge/models.py
+++ fudge/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
+    
+
+class BusInfoModel(models.Model):
+
+    bus_company = models.CharField(max_length=200)
+    bus_number = models.CharField(max_length=5)
+    bus_stop = models.CharField(max_length=300)
+    bus_time = models.TimeField()
