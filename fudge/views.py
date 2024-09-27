fb31cda80fda8f9ffe2b35735ba17bccbe25f5ea
Charles Mould <charles.mould.cm@gmail.com>
2024-09-27 12:07:13 +0000

    add view for bus info form

--- fudge/views.py
+++ fudge/views.py
@@ -6,6 +6,9 @@ from django.views.generic import TemplateView
 from django.contrib.auth import login 
 from .forms import CustomUserCreationForm # For user registratin
 from django.contrib.auth.decorators import login_required
+from .forms import BusInfoForm
+from .models import BusInfoModel
+#from .models import BusModel
 
 
 # View for homepage
@@ -36,6 +39,23 @@ def profile_view(request):
 def busreport_view(request):
     return render(request, 'bus/bus_report.html')
 
+# Bus route creation form
+@login_required
+def bus_report(request):
+    if request.method == 'POST':
+        form = BusInfoForm(request.POST)
+        if form.is_valid():
+            Content - form.cleaned_data['content']
+            return redirect('home')  # Redirect after successful submission
+    else:
+        form = BusInfoForm()
+    
+    return render(request, 'bus/bus_report.html', {'form': form})
 
 
+def bus_reports_view(request):
+    # Query the database for all bus reports
+    bus_reports = BusReport.objects.all()
 
+    # Pass the bus reports to the template
+    return render(request, 'bus_reports.html', {'bus_reports': bus_reports})
