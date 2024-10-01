c11ea29abe96a36bfe56a6fae682d7a32009745f
Charles Mould <charles.mould.cm@gmail.com>
2024-10-01 12:47:03 +0000

    add businfomodel and busstopmodel for appending more stops

--- fudge/models.py
+++ fudge/models.py
@@ -5,12 +5,32 @@ from django.urls import reverse
 # Create your models here.
     
 
-class BusInfoModel(models.Model):
+#class BusInfoModel(models.Model):
+#
+#    bus_company = models.CharField(max_length=200)
+#    bus_number = models.CharField(max_length=5)
 
-    bus_company = models.CharField(max_length=200)
-    bus_number = models.CharField(max_length=5)
+#class BusStopModel(models.Model):
+#    bus_info = models.ForeignKey(BusInfoModel, related_name="stops", on_delete=models.CASCADE)
+#    bus_stop = models.CharField(max_length=1000)
+#    bus_time = models.TimeField()
+
+
+class BusInfoModel(models.Model):
+    bus_company = models.CharField(max_length=200)  # Name of the bus company
+    bus_number = models.CharField(max_length=5)     # Number of the bus
+
+    def __str__(self):
+        return f"{self.bus_company} - {self.bus_number}"  # String representation for better readability
 
 class BusStopModel(models.Model):
-    bus_info = models.ForeignKey(BusInfoModel, related_name="stops", on_delete=models.CASCADE)
-    bus_stop = models.CharField(max_length=1000)
-    bus_time = models.TimeField()
+    bus_info = models.ForeignKey(
+        BusInfoModel,
+        related_name="stops",
+        on_delete=models.CASCADE
+    )
+    bus_stop = models.CharField(max_length=1000)  # Name or description of the bus stop
+    bus_time = models.TimeField()                   # Time the bus stops at this stop
+
+    def __str__(self):
+        return f"{self.bus_stop} at {self.bus_time}"  # String representation for better readability
