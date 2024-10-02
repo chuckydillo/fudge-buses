a7302794e554674fea68d5fbfc30219358d50332
Charles Mould <charles.mould.cm@gmail.com>
2024-10-02 17:08:30 +0000

    add views to view, edit and delete reports history

--- fudge/views.py
+++ fudge/views.py
@@ -1,7 +1,7 @@
 # Create your views here.
 
 
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import TemplateView
 from django.contrib.auth import login 
 from .forms import CustomUserCreationForm # For user registratin
@@ -68,7 +68,7 @@ def bus_add(request):
 # Form to report buses
 
 @login_required
-def bus_report_view(request):
+def user_bus_report_view(request):
     if request.method == 'POST':
         form = BusReportForm(request.POST)
         if form.is_valid():
@@ -80,4 +80,37 @@ def bus_report_view(request):
         form = BusReportForm()
 
    
-    return render(request, 'bus/bus_report.html', {'form': form})
+    return render(request, 'bus/user_bus_report.html', {'form': form})
+
+#
+
+@login_required
+def user_bus_report_history_view(request):
+    bus_reports = BusReportModel.objects.filter(submitted_by=request.user)
+
+    return render(request, 'bus/bus_report_history.html', {'user_bus_report_history_view': user_bus_report_history_view})
+
+@login_required
+def edit_bus_report_view(request, report_id):
+    bus_report = get_object_or_404(BusReportModel, id=report_id, submitted_by=request.user)
+
+
+    if request.method == 'POST':
+        form = BusReportForm(request.POST, instance=bus_report)
+        if form.is_valid():
+            form.save()
+            return redirect('bus_report')
+    else:
+        form = BusReportForm(instance=bus_report)
+
+    return render(request, 'bus/edit_bus_report.html', {'form': form, 'edit_bus_report': bus_report})
+
+@login_required
+def delete_bus_report_view(request, report_id):
+    bus_report = get_object_or_404(BusReportModel, id=report_id, submitted_by=request.user)
+    
+    if request.method == 'POST':
+        bus_report.delete()
+        return redirect('bus_report')
+
+    return render(request, 'bus/delete_bus_report.html', {'bus_report': bus_report})
