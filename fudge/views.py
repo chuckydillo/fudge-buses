28e60d7c8392129f0fa8d6644981e052e74a80c2
Charles Mould <charles.mould.cm@gmail.com>
2024-10-03 11:44:49 +0000

    tidy code, remove commented code, add comments with descriptions of code

--- fudge/views.py
+++ fudge/views.py
@@ -1,26 +1,23 @@
-# Create your views here.
-
-
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import TemplateView
-from django.contrib.auth import login
-from .forms import CustomUserCreationForm # For user registratin
-from django.contrib.auth.decorators import login_required
 from django.forms import modelformset_factory
+
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+
+from .forms import CustomUserCreationForm
 from .forms import BusInfoForm, BusStopForm, BusReportForm
 from .models import BusInfoModel, BusStopModel, BusReportModel
 
-from django.contrib.auth.decorators import user_passes_test
-#from .models import BusModel
-
+###################################
+# Account and admin related views #
+###################################
 def superuser_required(user):
     return user.is_superuser
 
 # View for homepage
 class HomePage(TemplateView):
-    """
-    Displays home page"
-    """
     template_name = 'index.html'
 
 # View for user registration
@@ -43,6 +40,10 @@ def account_success(request):
 @login_required
 def profile_view(request):
     return render(request, 'accounts/profile.html', {'user': request.user})
+
+#######################
+# Views for the admin #
+#######################
 
 # Fetch all bus info entries from the database
 @user_passes_test(superuser_required)
@@ -75,52 +76,55 @@ def bus_add(request):
 
     return render(request, 'bus/bus_add.html', {'bus_form': bus_form, 'formset': formset})
 
-# Form to report buses
 
+######################
+# Views for the user #
+######################
+
+# Form to report buses
 @login_required
 def user_bus_report_view(request):
     if request.method == 'POST':
         form = BusReportForm(request.POST)
         if form.is_valid():
-            bus_report = form.save(commit=False)
-            bus_report.submitted_by = request.user
-            bus_report.save()
-            return redirect('home')
+            user_bus_report = form.save(commit=False)
+            user_bus_report.submitted_by = request.user
+            user_bus_report.save()
+            return redirect('user_bus_report_history')
     else:
         form = BusReportForm()
 
    
     return render(request, 'bus/user_bus_report.html', {'form': form})
-
-#
-
+# Fetch all bus report history from the database
 @login_required
 def user_bus_report_history_view(request):
     bus_reports = BusReportModel.objects.filter(submitted_by=request.user)
 
-    return render(request, 'bus/bus_report_history.html', {'user_bus_report_history_view': user_bus_report_history_view})
+    return render(request, 'bus/bus_report_history.html', {'bus_reports': bus_reports})
 
+# View to edit bus routes
 @login_required
 def edit_bus_report_view(request, report_id):
     bus_report = get_object_or_404(BusReportModel, id=report_id, submitted_by=request.user)
-
 
     if request.method == 'POST':
         form = BusReportForm(request.POST, instance=bus_report)
         if form.is_valid():
             form.save()
-            return redirect('bus_report')
+            return redirect('user_bus_report_history')
     else:
         form = BusReportForm(instance=bus_report)
 
-    return render(request, 'bus/edit_bus_report.html', {'form': form, 'edit_bus_report': bus_report})
+    return render(request, 'bus/edit_bus_report.html', {'form': form, 'bus_report': bus_report})
 
+# View to delete bus routes
 @login_required
 def delete_bus_report_view(request, report_id):
     bus_report = get_object_or_404(BusReportModel, id=report_id, submitted_by=request.user)
     
     if request.method == 'POST':
         bus_report.delete()
-        return redirect('bus_report')
+        return redirect('user_bus_report_history')
 
     return render(request, 'bus/delete_bus_report.html', {'bus_report': bus_report})
