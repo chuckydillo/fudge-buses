1a484489dee9a3a52b6580d08f8e041fc1cf47df
Charles Mould <charles.mould.cm@gmail.com>
2024-10-03 14:32:07 +0000

    tidy code to Pep8 standards

--- fudge/urls.py
+++ fudge/urls.py
@@ -1,31 +1,72 @@
-from . import views
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import HomePage, register, user_bus_report_view, edit_bus_report_view, delete_bus_report_view, user_bus_report_history_view, AboutView, edit_bus_info_view, delete_bus_info_view
-
+from . import views
+from .views import (
+    HomePage, register, user_bus_report_view, edit_bus_report_view,
+    delete_bus_report_view, user_bus_report_history_view, AboutView,
+    edit_bus_info_view, delete_bus_info_view
+)
 
 urlpatterns = [
-    # Account related urls
-    path('', views.HomePage.as_view(), name='home'),  # Home page
-    path('register/', views.register, name='register'),  # Registration page
-    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),  # Login page
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),  # Logout page
+    # Account related URLs
+    path('', HomePage.as_view(), name='home'),  # Home page
+    path('register/', register, name='register'),  # Registration page
+    # Login page
+    path(
+        'login/',
+        auth_views.LoginView.as_view(template_name='accounts/login.html'),
+        name='login'
+    ),
+    # Logout page
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('profile/', views.profile_view, name='profile'),  # Profile page
-    path('account_success/', views.account_success, name='account_success'), # Account creation confirm page
+    # Account creation confirmation page
+    path(
+        'account_success/',
+        views.account_success,
+        name='account_success'
+    ),
 
-    # User urls
-    path('user-bus-report/', views.user_bus_report_view, name='user_bus_report'),  # User bus report page
-    path('user-bus-report-history/', views.user_bus_report_history_view, name='user_bus_report_history'),  # Bus report history page
-    path('edit-bus-report/<int:report_id>/', views.edit_bus_report_view, name='edit_bus_report'),  # Edit bus report page
-    path('delete-bus-report/<int:report_id>/', views.delete_bus_report_view, name='delete_bus_report'),  # Delete bus report page
-    
+    # User URLs
+    # User bus report page
+    path(
+        'user-bus-report/', user_bus_report_view, name='user_bus_report'
+    ),
+    # Bus report history page
+    path(
+        'user-bus-report-history/',
+        user_bus_report_history_view,
+        name='user_bus_report_history'
+    ),
+    # Edit bus report page
+    path(
+        'edit-bus-report/<int:report_id>/',
+        edit_bus_report_view,
+        name='edit_bus_report'
+    ),
+    # Delete bus report page
+    path(
+        'delete-bus-report/<int:report_id>/',
+        delete_bus_report_view,
+        name='delete_bus_report'
+    ),
 
-    # Admin urls
+    # Admin URLs
     path('bus-add/', views.bus_add, name='bus_add'),  # Add new bus report page
-    path('bus-info/', views.bus_info_list, name='bus_info_list'),  # Bus information list page
-    path('edit-bus-info/<int:bus_id>/', edit_bus_info_view, name='edit_bus_info'),  # Edit bus info page
-    path('delete-bus-info/<int:bus_id>/', delete_bus_info_view, name='delete_bus_info'),  # Delete bus info page
-    
-    # Misc urls
-    path('about/', AboutView.as_view(), name='about'),
+    path('bus-info/', views.bus_info_list, name='bus_info_list'),  # Bus info list page
+    # Edit bus info page
+    path(
+        'edit-bus-info/<int:bus_id>/',
+        edit_bus_info_view,
+        name='edit_bus_info'
+    ),
+    # Delete bus info page
+    path(
+        'delete-bus-info/<int:bus_id>/',
+        delete_bus_info_view,
+        name='delete_bus_info'
+    ),
+
+    # Misc URLs
+    path('about/', AboutView.as_view(), name='about'),  # About page
 ]
