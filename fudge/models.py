d4ccdfec9c89352bace7f98f1c0a8042e2d50987
Charles Mould <charles.mould.cm@gmail.com>
2024-10-03 14:34:12 +0000

    tidy code to ensure Pep8 standards

--- fudge/models.py
+++ fudge/models.py
@@ -3,10 +3,6 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 import datetime
 
-# Create your models here.
-    
-
-
 class BusInfoModel(models.Model):
     bus_company = models.CharField(max_length=200)  # Name of the bus company
     bus_number = models.CharField(max_length=5)     # Number of the bus
@@ -21,7 +17,7 @@ class BusStopModel(models.Model):
         on_delete=models.CASCADE
     )
     bus_stop = models.CharField(max_length=1000)  # Name or description of the bus stop
-    bus_time = models.TimeField()                   # Time the bus stops at this stop
+    bus_time = models.TimeField()                 # Time the bus stops at this stop
 
     def __str__(self):
         return f"{self.bus_stop} at {self.bus_time}"  # String representation for better readability
