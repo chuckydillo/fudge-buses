1b9250a539d9e701b82f92504a911228deeafb07
Charles Mould <charles.mould.cm@gmail.com>
2024-10-03 13:55:58 +0000

    Add urls for edit and delete views.

--- fudge/urls.py
+++ fudge/urls.py
@@ -1,7 +1,7 @@
 from . import views
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import HomePage, register, user_bus_report_view, edit_bus_report_view, delete_bus_report_view, user_bus_report_history_view, AboutView  
+from .views import HomePage, register, user_bus_report_view, edit_bus_report_view, delete_bus_report_view, user_bus_report_history_view, AboutView, edit_bus_info_view, delete_bus_info_view
 
 
 urlpatterns = [
@@ -18,11 +18,14 @@ urlpatterns = [
     path('user-bus-report-history/', views.user_bus_report_history_view, name='user_bus_report_history'),  # Bus report history page
     path('edit-bus-report/<int:report_id>/', views.edit_bus_report_view, name='edit_bus_report'),  # Edit bus report page
     path('delete-bus-report/<int:report_id>/', views.delete_bus_report_view, name='delete_bus_report'),  # Delete bus report page
+    
 
     # Admin urls
     path('bus-add/', views.bus_add, name='bus_add'),  # Add new bus report page
     path('bus-info/', views.bus_info_list, name='bus_info_list'),  # Bus information list page
-
+    path('edit-bus-info/<int:bus_id>/', edit_bus_info_view, name='edit_bus_info'),  # Edit bus info page
+    path('delete-bus-info/<int:bus_id>/', delete_bus_info_view, name='delete_bus_info'),  # Delete bus info page
+    
     # Misc urls
     path('about/', AboutView.as_view(), name='about'),
 ]
