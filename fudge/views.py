50c533d7d83752174d52697f1747b864bc858af8
Charles Mould <charles.mould.cm@gmail.com>
2024-10-03 14:35:42 +0000

    add views to edit and delete bus routes

--- fudge/views.py
+++ fudge/views.py
@@ -13,6 +13,8 @@ from .models import BusInfoModel, BusStopModel, BusReportModel
 ###################################
 # Account and admin related views #
 ###################################
+
+# Super User required
 def superuser_required(user):
     return user.is_superuser
 
@@ -76,6 +78,45 @@ def bus_add(request):
 
     return render(request, 'bus/bus_add.html', {'bus_form': bus_form, 'formset': formset})
 
+# View to edit bus routes
+@user_passes_test(superuser_required)
+def edit_bus_info_view(request, bus_id):
+    bus_info = get_object_or_404(BusInfoModel, id=bus_id)
+    BusStopModelFormSet = modelformset_factory(BusStopModel, form=BusStopForm, extra=1)
+
+    if request.method == 'POST':
+        bus_form = BusInfoForm(request.POST, instance=bus_info)
+        formset = BusStopModelFormSet(request.POST, queryset=bus_info.stops.all())
+
+        if bus_form.is_valid() and formset.is_valid():
+            bus_form.save()
+
+            for form in formset:
+                bus_stop = form.save(commit=False)
+                bus_stop.bus_info = bus_info
+                bus_stop.save()
+
+            return redirect('bus_info_list')
+    else:
+        bus_form = BusInfoForm(instance=bus_info)
+        formset = BusStopModelFormSet(queryset=bus_info.stops.all())
+
+    return render(request, 'bus/edit_bus_info.html', {
+        'bus_form': bus_form,
+        'formset': formset,
+    })
+
+# View to delete bus routes
+@user_passes_test(superuser_required)
+def delete_bus_info_view(request, bus_id):
+    bus_info = get_object_or_404(BusInfoModel, id=bus_id)
+
+    if request.method == 'POST':
+        bus_info.delete()
+        return redirect('bus_info_list')
+
+    return render(request, 'bus/delete_bus_info.html', {'bus_info': bus_info})
+
 
 ######################
 # Views for the user #
@@ -103,7 +144,7 @@ def user_bus_report_history_view(request):
 
     return render(request, 'bus/bus_report_history.html', {'bus_reports': bus_reports})
 
-# View to edit bus routes
+# View to edit bus reports
 @login_required
 def edit_bus_report_view(request, report_id):
     bus_report = get_object_or_404(BusReportModel, id=report_id, submitted_by=request.user)
@@ -118,7 +159,7 @@ def edit_bus_report_view(request, report_id):
 
     return render(request, 'bus/edit_bus_report.html', {'form': form, 'bus_report': bus_report})
 
-# View to delete bus routes
+# View to delete bus reports
 @login_required
 def delete_bus_report_view(request, report_id):
     bus_report = get_object_or_404(BusReportModel, id=report_id, submitted_by=request.user)
